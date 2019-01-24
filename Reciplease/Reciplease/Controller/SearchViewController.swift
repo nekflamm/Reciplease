645a4deb558239062d19dffb40358b15c955127d
nyoxide <nyoxide@hotmail.fr>
2019-01-24 18:48:17 +0100

    Initial commit

---
+++ Reciplease/Reciplease/Controller/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  Reciplease
+//
+//  Created by Adrien Carvalot on 24/01/2019.
+//  Copyright © 2019 Adrien Carvalot. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ Reciplease/Reciplease/View/SearchRecipesView.swift
@@ -0,0 +1,9 @@
+//
+//  SearchRecipesView.swift
+//  Reciplease
+//
+//  Created by Adrien Carvalot on 24/01/2019.
+//  Copyright © 2019 Adrien Carvalot. All rights reserved.
+//
+
+import Foundation
