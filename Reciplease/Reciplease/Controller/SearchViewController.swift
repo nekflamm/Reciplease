32a724da757e0143684815a465bd953798588179
nyoxide <nyoxide@hotmail.fr>
2019-01-25 13:13:08 +0100

    Adding animated banner

--- Reciplease/Reciplease/Controller/SearchViewController.swift
+++ Reciplease/Reciplease/Controller/SearchViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchRecipesView: SearchRecipesView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchRecipesView.scheduledTimerWithTimeInterval()
     }
-    
-    
 }
--- Reciplease/Reciplease/View/SearchRecipesView.swift
+++ Reciplease/Reciplease/View/SearchRecipesView.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import UIKit
+
+class SearchRecipesView: UIView {
+    
+    @IBOutlet weak var banner: UIImageView!
+    
+    private let imageArray = [UIImage(named: "pizza"), UIImage(named: "couscous"), UIImage(named: "sushis"), UIImage(named: "pates"), UIImage(named: "poivrons"), UIImage(named: "fruits")]
+    private var index = 0
+    
+    func scheduledTimerWithTimeInterval() {
+        _ = Timer.scheduledTimer(timeInterval: 7, target: self, selector: #selector(animImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func animImage() {
+        UIView.transition(with: banner, duration: 1.5, options: .transitionCrossDissolve, animations: {
+            self.banner.image = self.imageArray[self.checkIndex()]
+        }, completion: nil)
+    }
+    
+    private func checkIndex() -> Int {
+        index += 1
+        if index == imageArray.count {
+            index = 0
+        }
+        return index
+    }
+    
+}
