3ce40bcc1da745439700b6e4b99343194991a641
nyoxide <nyoxide@hotmail.fr>
2019-02-07 13:26:29 +0100

    Adding design on Recipes of the day page

--- Reciplease/Reciplease/Controller/TodaysRecipeViewController.swift
+++ Reciplease/Reciplease/Controller/TodaysRecipeViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class TodaysRecipeViewController: UIViewController {
 
+    @IBOutlet weak var mealsView: MealsView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func mealsButtons(_ sender: UIButton) {
+//        switch sender.tag {
+//        case 0:
+//            selectedImages.s
+//        case 1:
+//        case 2:
+//        case 3:
+//        default:
+//            break
+//        }
     }
-    */
-
 }
---
+++ Reciplease/Reciplease/View/MealsView.swift
@@ -0,0 +1,14 @@
+//
+//  MealsView.swift
+//  Reciplease
+//
+//  Created by Adrien Carvalot on 07/02/2019.
+//  Copyright © 2019 Adrien Carvalot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MealsView: UIStackView {
+    @IBOutlet var selectedImages: [UIImageView]!
+}
